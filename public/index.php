c2d722a704ec25ce16fce79fba30a1f0792e29bd
mevinod <connect@mevinod.com>
2020-04-22 11:49:14 +0530

    :tada: bootstrap

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="new.php"><strong>New bookmark</strong></a>Create new bookmark</li>
+	<li><a href="view.php"><strong>See my bookmarks</strong></a>See my bookmarks</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
---
+++ public/new.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Use an HTML form to create a new entry in the
+ * users table.
+ *
+ */
+
+require "../config.php";
+require "../common.php";
+
+if (isset($_POST['submit'])) {
+  if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
+
+  try  {
+    $connection = new PDO($dsn, $username, $password, $options);
+    
+    $new_user = array(
+      "name" => $_POST['name'],
+      "url"  => $_POST['url'],
+      "pin"     => $_POST['pin']
+    );
+
+    $sql = sprintf(
+      "INSERT INTO %s (%s) values (%s)",
+      "users",
+      implode(", ", array_keys($new_user)),
+      ":" . implode(", :", array_keys($new_user))
+    );
+    
+    $statement = $connection->prepare($sql);
+    $statement->execute($new_user);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+<?php require "templates/header.php"; ?>
+
+  <?php if (isset($_POST['submit']) && $statement) : ?>
+    <blockquote><?php echo escape($_POST['name']); ?> successfully added.</blockquote>
+  <?php endif; ?>
+
+  <h2>Add a user</h2>
+
+  <form method="post">
+    <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <label for="url">URL</label>
+    <input type="text" name="url" id="url">
+    <label for="pin">PIN</label>
+    <input type="text" name="pin" id="pin">
+    <input type="submit" name="submit" value="Submit">
+  </form>
+
+  <a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
